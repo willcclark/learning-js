841ce4e539c90c516e53300e4a931c54585cbccb
willcclark <67292935+willcclark@users.noreply.github.com will>
2020-11-28 10:47:01 -0500

    add files

---
+++ beginner-js-6.js
@@ -0,0 +1,18 @@
+// From: <https://www.youtube.com/watch?v=nQu2bbh4Vyc>
+// Example #1
+console.log('Example 1:');
+console.log('Hello World🌎');
+console.log(' ');
+
+// Example #2
+// Set some variables
+const place = "World";
+const greeting = "Hello";
+
+console.log('Example 2:');
+console.log('%s, %s,', greeting, place);
+console.log(' ');
+
+// Example #3 (Template literals - backticks)
+console.log('Example 3:');
+console.log(`${greeting}, ${place}`);
---
+++ beginner-js-8.js
@@ -0,0 +1,8 @@
+// console.log("Hello, infinity and beyond!")
+
+// -------------------------
+//  this code reverses a string
+
+/*
+Multi-line comments!!
+*/
